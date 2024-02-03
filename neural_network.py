a54b521061cd85160a4606af271a41af573024d8
PintoJav <javier.pinto@adidas.com>
2024-02-03 14:08:56 -0500

    PINTOIL-first_train_and_test_model

--- neural_network.py
+++ neural_network.py
@@ -1,19 +1,21 @@
 from keras.models import Sequential
 from keras.layers import Activation,Dropout,Flatten,Conv2D,MaxPool2D,Dense
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import numpy as np
 
 
 class Pintoil_Model:
 
-    def __init__(self,input_shape=(200,200,3), filters=33, kernel_size=(3,3), activation=['relu','sigmoid','softmax'], pool_size=(2,2),dense_layer_hiden=128, dense_layer_output=2,droup_layer=0.5):
+    def __init__(self,input_shape=(200,200,3), filters=33, kernel_size=(3,3), activation=['relu','sigmoid','softmax'], pool_size=(2,2),dense_layer_inter=60, dense_layer_output=2, layer_droupt=0.2):
         self.input_shape = input_shape
         self.filters = filters
         self.kernel_size = kernel_size
         self.activation = activation
         self.pool_size = pool_size
-        self.dense_layer_hiden = dense_layer_hiden
+        self.dense_layer_inter = dense_layer_inter
         self.dense_layer_output = dense_layer_output
-        self.droup_layer = droup_layer
+        self.layer_droupt = layer_droupt
 
     def model_architecture(self):
         model = Sequential()
@@ -31,15 +33,15 @@ class Pintoil_Model:
 
         model.add(Flatten())
 
-        model.add(Dense(self.dense_layer_hiden))
+        model.add(Dense(self.dense_layer_inter))
         model.add(Activation(self.activation[0]))
 
-        model.add(Dropout(self.droup_layer))
+        model.add(Dropout(self.layer_droupt))
 
         model.add(Dense(self.dense_layer_output))
-        model.add(Activation(self.activation[1]))
+        model.add(Activation(self.activation[0]))
 
-        model.compile(loss='binary_crossetropy',
+        model.compile(loss='binary_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
 
@@ -65,12 +67,20 @@ class Pintoil_Model:
                                            fill_mode=fill_node)
         return image_generator
 
-    def images_generator(self,images_path,batch_size=15,class_mode='binary'):
+    def images_generator(self, images_path,batch_size=15,class_mode='binary'):
         images_generate = self.images_data_generator()
-        image_generator_results=images_generate.flow_from_directory(images_path=images_path,target_size=self.input_shape[:2],batch_size=batch_size,class_mode=class_mode)
+        image_generator_results=images_generate.flow_from_directory(images_path,target_size=self.input_shape[:2],batch_size=batch_size,class_mode=class_mode)
         return image_generator_results
 
 
-    def model_results(self,train_images_generator,test_images_generator,epochs=5,step_per_epochs=150, validation_step=10):
-        results=self.model_architecture().fit_generator(train_images_generator=train_images_generator,epochs=epochs,step_per_epochs=step_per_epochs,validation_data=test_images_generator,validation_steps=validation_step)
+    def model_results(self, train_images_generator, test_images_generator, epochs=5, step_per_epochs=10, validation_step=3):
+        model = self.model_architecture()
+        results=model.fit_generator(train_images_generator,epochs=epochs,steps_per_epoch=step_per_epochs,validation_data=test_images_generator,validation_steps=validation_step)
         return results
+
+    def image_test_transformation(self, path):
+        image_test = image.load_img(path, target_size=self.input_shape[:2])
+        image_test = image.img_to_array(image_test)
+        image_test = np.expand_dims(image_test,axis=0)
+        image_test = image_test/255
+        return image_test
---
+++ train_test_model.py
@@ -0,0 +1,20 @@
+from neural_network import *
+
+# HERE I DECLARED THE INITAL ML MODEL
+model = Pintoil_Model()
+
+# HERE I AM GOIN TO GENERATE THE IMAGES
+path_train_data = './dataset1/train'
+path_test_data = "./dataset1/test"
+
+data_train_generate = model.images_generator(path_train_data)
+data_test_generate = model.images_generator(path_test_data)
+
+# HERE I WILL TRAIN ADN EVALUATE THE MODEL AT THE SAME TIME
+results_model = model.model_results(data_train_generate,data_test_generate)
+
+image_process1= model.image_test_transformation('/Users/pintojav/Downloads/dog_test_image.jpg')
+image_process2= model.image_test_transformation('/Users/pintojav/Downloads/famale_engineer.jpg')
+
+model_23=model.model_architecture().predict(image_process1)
+print(model_23)
